37810cdf7458432474e7a0a36eeceec3ac56bb42
kleber-a <kleberandrade1998@hotmail.com>
2023-03-03 02:06:31 -0300

    Criação da pageSkills

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  public list: Array<any> = [
+    {id: 1, nome: "teste"}, 
+    {id: 2, nome: "teste2"},
+    {id: 3, nome: "teste3"},
+    {id: 4, nome: "teste4"},
+    {id: 5, nome: "teste5"},
+    {id: 6, nome: "teste6"},
+  ]
+
+  public isSelected: boolean = false;
+  public description: string = "Clique em alguma tecnologia"
+
+  public selecionado : number = 0;
+
+
+  public change(){
+    if(this.selecionado > 0){
+      this.list.map((item)=>{
+          item.id === this.selecionado? this.description = item.nome: null;
+      })
+    }else{
+      this.description = "Clique em alguma tecnologia";
+    }
+  }
+
+  public selected(item:number, i: number){
+    if(item == this.selecionado && this.isSelected == true){
+      this.selecionado = 0;
+      this.isSelected = false;
+      this.change()
+    }else{
+      this.selecionado = item;
+      this.isSelected = true
+      this.change();
+    }
+
+    
+  }
+  
 }
